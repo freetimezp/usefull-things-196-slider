13fd47699af4053dac56e679393aa96e43b4c737
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-28 00:11:30 +0300

    03. usefull-things-196-slider. Data, shaders.

---
+++ src/assets/js/slides.js
@@ -0,0 +1,34 @@
+export const slides = [
+    {
+        title: "Quiet Green",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit aut nisi consequatur.",
+        type: "Editorial",
+        field: "Fine Art",
+        date: "2025",
+        image: "./src/assets/images/img-1.jpg",
+    },
+    {
+        title: "Grimson Reign",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit aut nisi consequatur.",
+        type: "Editorial",
+        field: "Conceptual",
+        date: "2024",
+        image: "./src/assets/images/img-2.jpg",
+    },
+    {
+        title: "Gilded Brow",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit aut nisi consequatur.",
+        type: "Detail Study",
+        field: "Experimental",
+        date: "2023",
+        image: "./src/assets/images/img-3.jpg",
+    },
+    {
+        title: "Dreamy Day",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit aut nisi consequatur.",
+        type: "Motion Style",
+        field: "Cinematic",
+        date: "2022",
+        image: "./src/assets/images/img-4.jpg",
+    },
+];
